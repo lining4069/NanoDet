9c89e697fa5f7f3585e7f092378bf17f005df6a8
lining4069 <13400164069@163.com>
2021-05-13 17:49:28 +0800

    基于NanDet人脸检测
    NanoDet-pytorch，数据集通过pycocotools加载COCO数据集训练模型保存再model文件中

---
+++ NanoDet/test.py
@@ -0,0 +1,10 @@
+# '''
+# '''目标检测-图片'''
+# python detect_main.py image --config ./config/nanodet-m.yml --model model/nanodet_m.pth --path  street.png
+#
+# '''目标检测-视频文件'''
+# python detect_main.py video --config ./config/nanodet-m.yml --model model/nanodet_m.pth --path  test.mp4
+#
+# '''目标检测-摄像头'''
+# python detect_main.py webcam --config ./config/nanodet-m.yml --model model/nanodet_m.pth --path  0
+# '''
